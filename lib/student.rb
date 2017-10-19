4e54176bdbd991818dbc52decec5faabe6bf100c
Timothy Cevallos <timcev@gmail.com>
2017-10-19 21:56:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,6 +16,10 @@ class Student
     sql = <<-SQL
     select *
     from students
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
